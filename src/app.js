89ce052d7a515a48fbacee7e407fd3dedebd8c5e
Wladson Ramos <wladsonramossi@gmail.com>
2024-05-20 10:17:58 -0300

    feat: add put method to update team data

--- src/app.js
+++ src/app.js
@@ -3,18 +3,32 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send(table2024);
 });
 
 app.get('/:acronym', (req, res) => {
-    const acronym = req.params.acronym.toUpperCase();
-    const team = table2024.find(infoTeam => infoTeam.acronym === acronym);
+    const informedAcronym = req.params.acronym.toUpperCase();
+    const team = table2024.find(infoTeam => infoTeam.acronym === informedAcronym);
     if (!team) {
         res.status(404).send("Time não encontrado");
         return;
     }
     res.status(200).send(team);
+});
+
+app.put('/:acronym', (req, res) => {
+    const informedAcronym = req.params.acronym.toUpperCase();
+    const selectedTeam = table2024.find((t) => t.acronym === informedAcronym);
+    const fields = Object.keys(req.body);
+
+    for(let field of fields) {
+        selectedTeam[field] = req.body[field];
+    }
+
+    res.status(200).send(selectedTeam);
 });
 
 app.listen(3000, () => console.log("Server is running"));
